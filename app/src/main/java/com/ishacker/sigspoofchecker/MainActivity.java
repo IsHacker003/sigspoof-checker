14154605da24d8edeed1ab451f8b258dd1017ce3
IsHacker <ishayu301@gmail.com>
2025-07-26 18:14:54 +0530

    Updated signature spoofing check method to use signingInfo

--- app/src/main/java/com/ishacker/sigspoofchecker/MainActivity.java
+++ app/src/main/java/com/ishacker/sigspoofchecker/MainActivity.java
@@ -3,6 +3,8 @@ package com.ishacker.sigspoofchecker;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.content.pm.SigningInfo;
+import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import com.google.common.io.BaseEncoding;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,14 +40,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        updateSpoofStatus();
+        try {
+            updateSpoofStatus();
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         requestSpoofPermission();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        updateSpoofStatus();
+        try {
+            updateSpoofStatus();
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // Request Signature Spoofing Runtime Permission
@@ -54,7 +70,11 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case SPOOF_PERMISSION_REQUEST_CODE: {
-                updateSpoofStatus();
+                try {
+                    updateSpoofStatus();
+                } catch (PackageManager.NameNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
                 return;
             }
         }
@@ -63,20 +83,48 @@ public class MainActivity extends AppCompatActivity {
 
     // Report Signature Spoofing Functional Status
 
-    protected void updateSpoofStatus() {
+    protected void updateSpoofStatus() throws PackageManager.NameNotFoundException {
         PackageInfo packageInfo;
         try {
             packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
         } catch (PackageManager.NameNotFoundException e) {
             throw new RuntimeException(e);
         }
-        Signature[] signatures = packageInfo.signatures;
-        if (signatures.length != 1) {
-            throw new RuntimeException("Got " + signatures.length + " signatures");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNING_CERTIFICATES);
+            String signatureString = String.valueOf(signatureDigest(packageInfo.signingInfo.getApkContentsSigners()));
+            Log.i(TAG, "Reported signature: " + signatureString);
+            setSpoofStatus(getString(R.string.fake_signature).equals(signatureString));
         }
-        String signatureString = signatures[0].toCharsString();
-        Log.i(TAG, "Reported signature: " + signatureString);
-        setSpoofStatus(getString(R.string.fake_signature).equals(signatureString));
+        else {
+            Signature[] signatures = packageInfo.signatures;
+            if (signatures.length != 1) {
+                throw new RuntimeException("Got " + signatures.length + " signatures");
+            }
+            String signatureString = signatures[0].toCharsString();
+            Log.i(TAG, "Reported signature: " + signatureString);
+            setSpoofStatus(getString(R.string.fake_signature).equals(signatureString));
+        }
+    }
+
+    private static String signatureDigest(Signature sig) {
+        byte[] signature = sig.toByteArray();
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] digest = md.digest(signature);
+            return BaseEncoding.base16().lowerCase().encode(digest);
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
+    private static List<String> signatureDigest(Signature[] sigList) {
+        List<String> signaturesList= new ArrayList<>();
+        for (Signature signature: sigList) {
+            if(signature!=null) {
+                signaturesList.add(signatureDigest(signature));
+            }
+        }
+        return signaturesList;
     }
 
     protected void setSpoofStatus(boolean enabled) {
